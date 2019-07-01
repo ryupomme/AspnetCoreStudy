8feeca37ff1a2112949c15d364bde1da6e15bc47
ghryu <ghryu@mmonstar.co.kr>
2019-07-01 11:24:05 +0900

    회원가입 form test validation message test

---
+++ AspnetCoreStudy/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspnetCoreStudy.DataContext;
+using AspnetCoreStudy.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspnetCoreStudy.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /<controller>/
+        // allocation이 없으면 default로 HttpGet 이다
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User model)
+        {
+            //validation check
+
+            if (ModelState.IsValid)
+            {
+                //db입출력할때 입력서 open connection, close connection. 매번 작성하지 않고 using문 사용
+                // Java -> try(SqlSession){} catch{}
+                using(var db = new AspnetStudyDBContext())
+                {
+                    db.Users.Add(model);        // memory에
+                    db.SaveChanges();           // DB에 반영
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+    }
+}
--- AspnetCoreStudy/Models/User.cs
+++ AspnetCoreStudy/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,17 @@ namespace AspnetCoreStudy.Models
 {
     public class User
     {
-        // user number
+        [Key]       // PK // user number
         public int No { get; set; }
 
+        [Required(ErrorMessage = "사용자 이름을 입력하세요.")]
         // user name
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "사용자 아이디을 입력하세요.")]
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "사용자 비밀번호를 입력하세요.")]
+        public int Password { get; set; }
     }
 }
